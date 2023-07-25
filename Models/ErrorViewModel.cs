357c1ad72369c450a93f87fc84c73e56552468a8
Tyler Strel <Tyler.Strel@gmail.com>
2023-07-25 15:55:58 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ContactHarbor.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
