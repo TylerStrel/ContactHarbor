a5b435e2e9dafe0fe81b23d4c2a261c67bc12825
Tyler Strel <Tyler.Strel@gmail.com>
2023-08-02 22:25:20 -0400

    Fixed an error with the EmailService

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -2,6 +2,7 @@
 using MailKit.Net.Smtp;
 using MailKit.Security;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
 using MimeKit;
 
 
@@ -9,13 +10,11 @@ namespace ContactHarbor.Services;
 
 public class EmailService : IEmailSender
 {
-    private readonly IConfiguration _configuration;
     private readonly MailSettings _mailSettings;
 
-    public EmailService(IConfiguration configuration, MailSettings mailSettings)
+    public EmailService(IOptions<MailSettings> mailSettings)
     {
-        _configuration = configuration;
-        _mailSettings = mailSettings;
+        _mailSettings = mailSettings.Value;
     }
 
     public async Task SendEmailAsync(string email, string subject, string htmlMessage)
